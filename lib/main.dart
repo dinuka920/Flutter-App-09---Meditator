965e9af645265847bb67ea4274b8bee34a7c42f0
Dinuka Hasanka <dinukahasanka920@gmail.com>
2025-03-12 00:03:03 +0530

    setting up Hive Local Storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:meditator_app/models/meditation_exercise_model.dart';
+import 'package:meditator_app/models/mindfulness_exercise_model.dart';
+import 'package:meditator_app/models/sleep_exercise_model.dart';
+import 'package:meditator_app/router/router.dart';
+
+// providers
+import 'package:provider/provider.dart';
 import 'package:meditator_app/providers/filter_provider.dart';
 import 'package:meditator_app/providers/meditation_exercise_provider.dart';
 import 'package:meditator_app/providers/mindfull_exercise_provider.dart';
 import 'package:meditator_app/providers/sleep_exercise_provider.dart';
-import 'package:meditator_app/router/router.dart';
-import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(MeditationExerciseModelAdapter());
+  Hive.registerAdapter(MindfulnessExerciseModelAdapter());
+  Hive.registerAdapter(SleepExerciseModelAdapter());
+
+  await Hive.openBox("meditation_data");
+  await Hive.openBox("mindfulness_data");
+  await Hive.openBox("sleep_data");
+
   runApp(
     MultiProvider(
       providers: [
